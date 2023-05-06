fe715003c5314d007d41dfb8dd30368ada1930f7
Evgeny <120304735+BratkovskyEvgeny@users.noreply.github.com>
2023-05-06 14:56:39 +0300

    Update serg.py

--- serg.py
+++ serg.py
@@ -7,7 +7,7 @@ from streamlit_player import st_player
 
 
 rain(
-    emoji="💵",
+    emoji="💶",
     font_size=30,
     falling_speed=5,
     animation_length="infinite",
@@ -210,7 +210,7 @@ def main(
 
     #def example():
         #rain(
-            #emoji="💵",
+            #emoji="💶",
             #font_size=30,
             #falling_speed=5,
             #animation_length="infinite",
